ca611313d00fd0bd050ae59ebce09fcbdd65d594
tilpner <till@hoeppner.ws>
2018-09-06 18:52:20 +0200

    Add rust overlay to nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -5,5 +5,14 @@ import <nixpkgs> {
         url = https://github.com/nix-community/NUR/archive/master.tar.gz;
       })) {};
     })
+
+    (import "${builtins.fetchTarball {
+      url = https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz;
+    }}/rust-overlay.nix")
+
+    (self: super: {
+      inherit (super.nur.repos.tilpner.pkgs)
+        bitflip kernelConfig;
+    })
   ];
 }
